444fada19214d83313d5306179ba8a1d154be854
Aniket Ozarkar <ozarkaraniket@gmail.com>
2021-09-01 18:03:53 +0530

    feat: css added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import { useState } from 'react';
 
 var emojiDictionary = {
-  "🖤": { name: "Black Heart", meaning: "A heart shaded completely black. May be used to express morbidity, sorrow, or a form of dark humor, but despite the color remains most commonly used for love and affection."},
-  "💚": { name: "Green Heart", meaning: "A green heart emoji, often used alongside other colored hearts. Frequently used on Twitter in reference to Korean boy band NCT."},
-  "💙": { name: "Blue Heart", meaning: "A blue heart emoji, commonly used by brands as a neutral heart color."},
-  "🤍": { name: "White Heart", meaning: "A white heart emoji, used for love and affection. Also commonly used to discuss someone passing away."},
-  "💜": { name: "Purple Heart", meaning: "A purple heart emoji, often used alongside other colored hearts. Frequently used on Twitter in reference to Korean boy band Bangtan Sonyeondan, more commonly known as BTS."},
-  "🤎": { name: "Brown Heart", meaning: "A brown heart, often used discuss topics related to racial identity 🧑🏽🧑🏾🧑🏿"},
-  "💛": { name: "Yellow Heart", meaning: "A yellow or gold colored love-heart, frequently used alongside other colored hearts."},
-  "🧡": { name: "Orange Heart", meaning: "No Meaning found"},
-  "❤️": { name: "Red Heart", meaning: "A classic red love heart emoji, used for expressions of love and romance. This is the most popular heart emoji."}
+  "🖤": { name: "Black", meaning: "A heart shaded completely black. May be used to express morbidity, sorrow, or a form of dark humor, but despite the color remains most commonly used for love and affection."},
+  "💚": { name: "Green", meaning: "A green heart emoji, often used alongside other colored hearts. Frequently used on Twitter in reference to Korean boy band NCT."},
+  "💙": { name: "Blue", meaning: "A blue heart emoji, commonly used by brands as a neutral heart color."},
+  "🤍": { name: "White", meaning: "A white heart emoji, used for love and affection. Also commonly used to discuss someone passing away."},
+  "💜": { name: "Purple", meaning: "A purple heart emoji, often used alongside other colored hearts. Frequently used on Twitter in reference to Korean boy band Bangtan Sonyeondan, more commonly known as BTS."},
+  "🤎": { name: "Brown", meaning: "A brown heart, often used discuss topics related to racial identity 🧑🏽🧑🏾🧑🏿"},
+  "💛": { name: "Yellow", meaning: "A yellow or gold colored love-heart, frequently used alongside other colored hearts."},
+  "🧡": { name: "Orange", meaning: "No Meaning found"},
+  "❤️": { name: "Red", meaning: "A classic red love heart emoji, used for expressions of love and romance. This is the most popular heart emoji."}
   };
 
 var knownEmojies = Object.keys(emojiDictionary);
@@ -55,11 +55,11 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Emoji Dictionary</h1>
-      <input onChange={ emojiChangeHandler }></input>
-      <h2>{name}</h2>
+      <h1 style={{margin:"1rem 1rem"}}>Emoji Dictionary</h1>
+      <input className="emojiInput" onChange={ emojiChangeHandler }></input>
+      <h2>{name} Heart</h2>
       <div>{meaning}</div>
-      <ul>
+      <ul style={{ paddingInlineStart: "0" }}>
         {
           knownEmojies.map((emoji) =>
             <span 
